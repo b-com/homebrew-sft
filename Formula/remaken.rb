e764de498163d4d93a9145eb61c11fbfed9136ee
Loïc TOURAINE <loic.touraine@b-com.com>
2020-03-19 11:27:17 +0100

    Add remaken formula

---
+++ Formula/remaken.rb
@@ -0,0 +1,11 @@
+class remaken < Formula
+  desc "usefull tool that handles binary dependencies from various package managers (conan, vcpkg, system package managers) or from bare repositories with remaken own package format."
+  homepage "https://github.com/b-com-software-basis/remaken"
+
+
+  url "https://github.com/b-com-software-basis/remaken/releases/download/Ubuntu1804%2Fremaken-1.5.2/remaken", :using => :curl
+
+  def install
+    bin.install "b-com/remaken"
+  end
+end
