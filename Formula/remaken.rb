20b068ac65c983316f451e61865b36c358a0a54d
Loïc TOURAINE <loic.touraine@b-com.com>
2020-03-26 16:46:28 +0100

    fix: add linux sha256

--- Formula/remaken.rb
+++ Formula/remaken.rb
@@ -8,6 +8,7 @@ class Remaken < Formula
       sha256 "9a4d0e28e8ef23411f1aaa1c5960f5b5986b516df3c84cacd7679b08f1ded513"
     elsif OS.linux?
       url "https://github.com/b-com-software-basis/remaken/releases/download/Ubuntu1804%2Fremaken-1.5.2/remaken", :using => :curl
+      sha256 "debd38ebf4670bc73da066f789f3509ddb67d692456eca851aa6e1a106e70e14"
     end
 
     resource "qmake" do
