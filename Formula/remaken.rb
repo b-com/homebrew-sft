13224f54ea9b5cac5dddc719c1099c65cda4a279
Loïc TOURAINE <loic.touraine@b-com.com>
2020-05-11 14:54:36 +0200

    update: version to 1.6.2 for both mac and linux feat: add cmake as a recommended dependency

--- Formula/remaken.rb
+++ Formula/remaken.rb
@@ -1,18 +1,19 @@
 class Remaken < Formula
-  desc "usefull tool that handles binary dependencies from various package managers (conan, vcpkg, system package managers) or from bare repositories with remaken own package format."
+  desc "useful tool that handles binary dependencies from various package managers (conan, vcpkg, system package managers) or from bare repositories with remaken own package format."
   homepage "https://github.com/b-com-software-basis/remaken"
 
 
     if OS.mac?
-      url "https://github.com/b-com-software-basis/remaken/releases/download/macOS-Mojave%2Fremaken-1.6.1/remaken", :using => :curl
-      sha256 "c7ff6130d4b9e7efa270d505ff430709bdc1971ee012b92e61f5f40d2dcf01b7"
+      url "https://github.com/b-com-software-basis/remaken/releases/download/macOS-Mojave%2Fremaken-1.6.2/remaken", :using => :curl
+      sha256 "a8884e2833135a01d4effa894aad8a2b7d310f02c9fc68082b047dc213ce6068"
       depends_on "pkg-config" => :recommended
     elsif OS.linux?
-      url "https://github.com/b-com-software-basis/remaken/releases/download/Ubuntu1804%2Fremaken-1.6.0/remaken", :using => :curl
-      sha256 "feb08af3f89355d866467b65e44377bfe17bee87c34f8c90dacda52e47a87cad"
+      url "https://github.com/b-com-software-basis/remaken/releases/download/Ubuntu1804%2Fremaken-1.6.2/remaken", :using => :curl
+      sha256 "d279de99be83d5b45d21949e6bc9d9615ff0d22e03b800e70b3b0d8069482079"
     end
   
   depends_on "conan" => :recommended
+  depends_on "cmake" => :recommended
 
   def install
     bin.install "remaken"
